1650d6b3b790c661493e9b9d075ca64d7b181897
joebrew <joebrew@gmail.com>
2018-04-03 12:56:25 +0200

    app for oleksiy

--- app.R
+++ app.R
@@ -1,124 +1,147 @@
-# Oleksiy Anokhin
-# March 7, 2018
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
-# Visualizing DC crimes in 2017 through Shiny app
-
-#------------
-# Global Code
-#------------
-
-# Install packages
 library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(leaflet)
 library(shinythemes)
-
-# Set working directory
-setwd("C:/Users/wb516241/IFC/!My Shiny apps/App DC Crimes 2017")
+library(knitr)
+library(kableExtra)
+library(RColorBrewer)
 
 # Read the initial file
 incidents <- read.csv("Crime_Incidents_in_2017.csv", header = TRUE, stringsAsFactors = FALSE)
 
+# Define function for legend
+addLegendCustom <- function(map, colors, labels, sizes, opacity = 0.5, ...){
+  colorAdditions <- paste0(colors, "; width:", sizes, "px; height:", sizes, "px")
+  labelAdditions <- paste0("<div style='display: inline-block;height: ", sizes, "px;margin-top: 4px;line-height: ", sizes, "px;'>", labels, "</div>")
+  
+  return(addLegend(map, colors = colorAdditions, labels = labelAdditions, opacity = opacity, ...))
+}
 
 # User interface
 ui <- fluidPage(theme = shinytheme("united"),
                 titlePanel("Crimes in Washington, DC (2017)"), 
-                sidebarLayout(
-                  sidebarPanel(
-                    selectInput("offenceInput", "Type of Offence",
-                                choices = c("Choose type", 
-                                            "Arson",
-                                            "Assault w/danderous weapon",
-                                            "Burglary",
-                                            "Homicide",
-                                            "Motor vehicle theft",
-                                            "Robbery", 
-                                            "Sex abuse",
-                                            "Theft f/auto",
-                                            "Theft/Other"),
-                                selected = "Choose type"),
-                    selectInput("methodInput", "Method of Offence",
-                                choices = c("Choose method", 
-                                            "Gun",
-                                            "Knife"),
-                                selected = "Choose method"),
-                    selectInput("shiftInput", "Police Shift",
-                                choices = c("Choose shift", 
-                                            "Day", 
-                                            "Evening", 
-                                            "Midnight"),
-                                selected = "Choose shift")
-                  ),
-                  
-                  mainPanel(leafletOutput(outputId = 'map', height = 800) 
-                  )
+                fluidRow(column(4,
+                                selectInput("offenceInput", "Type of Offence",
+                                            choices = sort(unique(incidents$OFFENSE)),
+                                            selected = sort(unique(incidents$OFFENSE)),
+                                            multiple = TRUE),
+                                selectInput("methodInput", "Method of Offence",
+                                            choices = sort(unique(incidents$METHOD)),
+                                            selected = sort(unique(incidents$METHOD)),
+                                            multiple = TRUE),
+                                selectInput("shiftInput", "Police Shift",
+                                            choices = sort(unique(incidents$SHIFT)),
+                                            selected = sort(unique(incidents$SHIFT)),
+                                            multiple = TRUE)
+                ),
+                
+                column(8,
+                       leafletOutput(outputId = 'map', height = 600)
                 )
+                )
+                
 )
 
-
-# Server 
-
-server <- shinyServer(function(input, output) {
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {  
+  
+  # Filter the data based on inputs
+  filtered_data <- reactive({
+    selected_offence <- input$offenceInput
+    selected_method <- input$methodInput
+    selected_shift <- input$shiftInput
+    
+    out <- incidents
+    
+    # Offense filtering
+    
+    if(!is.null(selected_offence)){
+      if(!all(selected_offence == '')){
+        message('Keeping the following offences:')
+        message(paste0('---', selected_offence, '\n', collapse = ''))
+        out <- out %>%
+          filter(OFFENSE %in% selected_offence)
+      }
+      
+    }
+    
+    # Method filtering filtering
+    if(!is.null(selected_method)){
+      if(!all(selected_method == '')){
+        message('Keeping the following methods:')
+        message(paste0('---', selected_method, '\n', collapse = ''))
+        out <- out %>%
+          filter(METHOD %in% selected_method)
+      }
+    }
+    
+    # Shift filtering 
+    if(!is.null(selected_shift)){
+      if(!all(selected_shift == '')){
+        message('Keeping the following shifts:')
+        message(paste0('---', selected_shift, '\n', collapse = ''))
+        out <- out %>%
+          filter(SHIFT %in% selected_shift)
+      }
+    }
+    
+    message('Filtered data has ', nrow(out), ' rows.')
+    return(out)
+  })
+  
   output$map <- renderLeaflet({
-    leaflet(incidents) %>% 
-      addProviderTiles(providers$Stamen.Terrain) %>% 
-      setView(-77.0369, 38.9072, zoom = 12) #%>%
     
-  })
-  # observers
-  
-  # selected offence
-  selectedOffense <- reactive({ # SO THIS LINE IS A PROBLEM
-    incidents[incidents$OFFENSE == input$offenceInput, ] 
-    # tmp <- incidents[!is.na(incidents$OFFENCE),]
-    #tmp[tmp$OFFENCE == input$offenceInput, ] 
-  })
-  # AND SURPRISINGLY POP-UP 
-  # state_popup1 <- paste0("<strong>Type of offence: </strong>", 
-  #                       selectedOffence()$OFFENCE, 
-  #                       "<br><strong> Method of Offence: </strong>", 
-  #                       selectedOffence()$METHOD,
-  #                       "<br><strong> Police shift: </strong>", 
-  #                       selectedOffence()$SHIFT)
-  observe({
     
-    leafletProxy("map", data =  selectedOffense()) %>%
-      clearShapes() %>%
-      addCircles(lng = incidents$Lon, lat = incidents$Lat, weight = 1,
-               color = "red", radius = 10, opacity = 0.9#, popup = ~state_popup1
-               ) 
-  })
-  
-  selectedMethod <- reactive({ # SO THIS LINE IS A PROBLEM
-    incidents[incidents$METHOD == input$methodInput, ] 
-    # tmp <- incidents[!is.na(incidents$OFFENCE),]
-    #tmp[tmp$OFFENCE == input$offenceInput, ] 
-  })
- 
-  observe({
+    # Get the filtered data first
+    df <- filtered_data()
     
-    leafletProxy("map", data =  selectedMethod()) %>%
-      clearShapes() %>%
-      addCircles(lng = incidents$Lon, lat = incidents$Lat, weight = 1,
-                 color = "red", radius = 10, opacity = 0.9#, popup = ~state_popup1
-      ) 
+    # If there is any data, carry on
+    if(nrow(df) > 0){
+      
+      l <- 
+        leaflet(data = df) %>% 
+        addProviderTiles(providers$Stamen.Toner) %>% 
+        setView(-77.0369, 38.9072, zoom = 12)
+      
+      message(nrow(df), ' crimes filtered.')
+      
+      # Define a color vector
+      color_vector <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = 'Spectral'))(length(unique(df$OFFENSE)))
+      color_labels <- sort(unique(df$OFFENSE))
+      pal <- colorFactor(
+        color_vector,
+        domain = color_labels)
+      
+      l <- l %>%
+        addCircles(lng = df$Lon, lat = df$Lat, weight = 1,
+                   popup = paste0(df$OFFENSE, ' at ', df$BLOCK),
+                   color = ~pal(df$OFFENSE),
+                   radius = 20, opacity = 0.9) %>%
+        addLegendCustom(colors = color_vector, 
+                        labels = color_labels, sizes = rep(20, length(color_vector)),
+                        position = 'bottomright')
+      
+    } else {
+      message('No crimes with current filter settings.')
+      l <- l <- 
+        leaflet() %>% 
+        addProviderTiles(providers$Stamen.Toner) %>% 
+        setView(-77.0369, 38.9072, zoom = 12)
+    }
+    return(l)
   })
-  
-  selectedShift <- reactive({ # SO THIS LINE IS A PROBLEM
-    incidents[incidents$SHIFT == input$shiftInput, ] 
-    # tmp <- incidents[!is.na(incidents$OFFENCE),]
-    #tmp[tmp$OFFENCE == input$offenceInput, ] 
-  })
-  
-  observe({
-    
-    leafletProxy("map", data =  selectedShift()) %>%
-      clearShapes() %>%
-      addCircles(lng = incidents$Lon, lat = incidents$Lat, weight = 1,
-                 color = "red", radius = 10, opacity = 0.9#, popup = ~state_popup1
-      ) 
-  })
-})
+}
 
+# Run the application 
 shinyApp(ui = ui, server = server)
+
